c2c4701c949a9c70e789cbdbc21e6cfc7f516e00
neutron2025 <1229507902@qq.com>
2023-03-15 11:22:40 +0800

    quant

--- src/App.js
+++ src/App.js
@@ -4,20 +4,16 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <body>
+      <video width="980" height="620" controls>
+      <source src="quant.mp4" type="video/mp4"/>
+</video>
+      <h3>依照数据策略，自动下单，撤单，止损</h3>
+{/* <iframe src="quant.mp4" quality="high" width="680" height="400" align="middle" allowScriptAccess="sameDomain" type="application/x-shockwave-flash"></iframe>` */}
+{/* <video autoplay="autoplay" loop="loop" id="video" x-webkit-airplay="true" webkit-playsinline="true" muted="">
+<source src="quant.mp4" type="video/mp4"/>
+</video> */}
+      </body>
     </div>
   );
 }
